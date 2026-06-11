10056b22c35dbdcc00be5be97a09246c190c0d25
newkub <newkubise@gmail.com>
2026-06-12 04:04:10 +0700

    fix(app): update App.tsx routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { Router } from '@solidjs/router';
-import MainLayout from './components/MainLayout';
 
 export default function App() {
   return (
     <div class="flex h-screen bg-bg-primary text-text-primary">
-      <Router>
-        <MainLayout />
-      </Router>
+      <Router />
     </div>
   );
 }
